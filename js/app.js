4b360bf37001f5196fac8ea4bdc3dc1a683798a0
alanllamas <alanllamasg@gmail.com>
2015-03-20 14:37:36 -0600

    se implementa angular-wizard

--- js/app.js
+++ js/app.js
@@ -1,64 +1,23 @@
-(function () {
-	var app = angular.module('wizard', []);
+var app = angular.module('myApp', ['mgo-angular-wizard']);
 
-	app.controller('MainCtrl', ['$scope', function ($scope) {
-		this.step = 0;
-		this.currStep = function (curr) {
-			if (curr === 1) {
-				return this.step = curr;
-			};
-			if (curr === 2) {
-				return this.step = curr;
-			};
-			if (curr === 3) {
-				return this.step = curr;
-			};
-			if (curr === 4) {
-				return this.step = curr;
-			};
-			if (curr === 5) {
-				return this.step = curr;
-			};
-			if (curr === 6) {
-				return this.step = 6;
-			};
-		};
-	}]);
+	app.controller('MainCtrl', ['$scope','WizardHandler', function ($scope, WizardHandler) {
+			
 
-	app.directive('cuenta', [function () {
-		return {
-			restrict: 'EA',
-			templateUrl: 'templates/cuenta.html'
+		$scope.pass = function () {
+			
+			for (var i = 0; i < cuenta.length; i++) {
+				if (cuenta[i].type != 'submit') {
+
+					console.log(cuenta[i].type)
+				};
+			};
+
+
+
+			return true; 
+			
 		};
+
+
+
 	}]);
-	app.directive('personal', [function () {
-		return {
-			restrict: 'EA',
-			templateUrl: 'templates/personal.html'
-		};
-	}]);
-	app.directive('estudios', [function () {
-		return {
-			restrict: 'EA',
-			templateUrl: 'templates/estudios.html'
-		};
-	}]);
-	app.directive('buscas', [function () {
-		return {
-			restrict: 'EA',
-			templateUrl: 'templates/buscas.html'
-		};
-	}]);
-	app.directive('membresia', [function () {
-		return {
-			restrict: 'EA',
-			templateUrl: 'templates/membresia.html'
-		};
-	}]);
-	app.directive('fotografia', [function () {
-		return {
-			restrict: 'EA',
-			templateUrl: 'templates/fotografia.html'
-		};
-	}]);
-})();
