8d7da1e543a0ede3818cb517fe4a161310ff0a60
alanllamas <alanllamasg@gmail.com>
2015-03-26 23:51:55 -0600

    se agrega validacion hasta foto

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ var app = angular.module('myApp', ['mgo-angular-wizard', 'ui.validate', ]);
 	app.controller('MainCtrl', ['$scope','WizardHandler','$log', function ($scope, WizardHandler, $log) {
 		this.sexo;
 		this.busco;
-
-			info = [];
+		info = [{},{},{},{},{}];
+		this.info =  info;
 			
 		this.store = function  () {
 			if (!store.enabled) {
@@ -12,101 +12,142 @@ var app = angular.module('myApp', ['mgo-angular-wizard', 'ui.validate', ]);
 				return
 			}
 		};
-	/*
 		$scope.validate = function (data, id){
 		    if (data) {
 				validate = /^[0-9]*$/.test(data);
 				$log.log(validate);
-					id = $('#' + id)
+					id = $('#' + id);
+					btn = $('#pers');
+					if (!validate) {
+						
+						id.addClass('ng-invalid');
+						id.addClass('ng-touched');
+					}else{
+						id.removeClass('ng-invalid');
+						
+					};
 					$log.log(data, id);
 				   	return validate ;
 
 		    };
 		}; 
-			 <!--
-	    $scope.validate = function (evt){
-	         var charCode = (evt.which) ? evt.which : event.keyCode
-	         if (charCode > 31 && (charCode < 48 || charCode > 57))
-	            return false;
 
-	         return true;
-	      }
-	      //-->
-		*/
 		$scope.pass = function () {
 
 			form =$('form');
+			check = [];
+			check2 = [];
 
 			C = WizardHandler.wizard('wizard').currentStepNumber()-1;
 			f = form[C];
-			function Arr (f) {
-				F = {
-					f : {}
-				};
-				return F; 
-			};
-			Arr(f);
+				console.log(f.name);
+			if (f.name != "foto") {
 
-			for (var i = 0; i < f.length; i++) {
-			
-
-					if (f[i].type === 'radio') {
-						r = angular.element(f[i]).hasClass('ng-valid-parse');
-						if (r) {
-							if (indexOf(f.name) == -1) {
-								F.push(f[i].value);
-							};
-							
-						};
-					};
-					if (f[i].type === 'checkbox') {
-
-						g = angular.element(f[i]).hasClass('gay');
-						if (g && f[i].checked) {
-							 
-							if (indexOf(f.name) == -1) {
-								F.push(f[i].value);
-							};
-						};
-
-						
-						d1 = angular.element(f[i]).hasClass('day1');
-						if (d1 && f[i].checked) {
-							if (indexOf(f.name) == -1) {
-								F.push(f[i].value);
-							};
-						};
-
-
-						d2 = angular.element(f[i]).hasClass('day2');
-						if (d2 && f[i].checked) {
-							if (indexOf(f.name) == -1) {
-								F.push(f[i].value);
-							};
-						};
-					};
-					if (f[i].name != 'confirm' && f[i].type != 'submit' && f[i].type != 'radio' && f[i].type != 'checkbox' ) {
-						
-							if (indexOf(f.name) == -1) {
-								F.push(f[i].value);
-							};
-
-
-						if (f[i].value != "") {
-						};
-
-					};
-			
-						 
+				for (var i = 0; i < f.length; i++) {
 				
-			};
-				console.log(F);
-			if (C === 4) {
 
-				get = store.getAll();
-				info.push(get);
-				console.log(info);
+						if (f[i].type === 'radio') {
+							r = angular.element(f[i]).hasClass('ng-valid-parse');
+							if (r) {
+								v =	f[i].value;
+								n = f[i].name;
+								info[C][n]= v ;
+								
+							};
+						};
+						if (f[i].type === 'checkbox') {
+
+							g = angular.element(f[i]).hasClass('gay');
+							if (g) {
+								 
+								v =	f[i].value;
+								n = f[i].name;
+								info[C][v]= f[i].checked ;
+							};
+
+							
+							d1 = angular.element(f[i]).hasClass('day1');
+							D1 = $('.day1');
+							if (d1) {
+								if (f[i].checked) {
+									check.push(1);
+								}else{
+									check.push(0);
+								};
+									v =	f[i].value;
+									n = f[i].name;
+									if (check.length == 7 && check.indexOf(1) == -1) {
+										D1.addClass('ng-invalid');
+										D1.addClass('ng-touched');
+
+										D1.removeClass('ng-valid');
+										D1.removeClass('ng-untouched');
+										return false;
+									}else{
+										D1.addClass('ng-valid');
+										
+
+										D1.removeClass('ng-invalid');
+									};
+								info[C][v + "1"]= f[i].checked ;
+							};
+
+
+							d2 = angular.element(f[i]).hasClass('day2');
+							D2 = $('.day2');
+							if (d2) {
+								if (f[i].checked) {
+									check2.push(1);
+								}else{
+									check2.push(0);
+								};
+									v =	f[i].value;
+									n = f[i].name;
+									if (check2.length == 7 && check2.indexOf(1) == -1) {
+										D2.addClass('ng-invalid');
+										D2.addClass('ng-touched');
+
+										D2.removeClass('ng-valid');
+										D2.removeClass('ng-untouched');
+										return false;
+									}else{
+										D2.addClass('ng-valid');
+										D2.removeClass('ng-invalid');
+									};
+								info[C][v + "2"]= f[i].checked ;
+							};
+						};
+						if (angular.element(f[i]).hasClass('tel1')) {
+							tel = [f['tel1_tipo'].value,f['lada'].value,f['phone'].value];
+							info[C]['tel'] = tel;
+
+						};
+						if (angular.element(f[i]).hasClass('tel1')) {
+							tel = [f['tel2_tipo'].value,f['lada2'].value,f['phone2'].value];
+							info[C]['tel2'] = tel;
+						};
+						if (f[i].name != 'confirm' && f[i].type != 'submit' && f[i].type != 'radio' && f[i].type != 'checkbox' && !angular.element(f[i]).hasClass('tel1') && !angular.element(f[i]).hasClass('tel2') ) {
+							
+								
+								v =	f[i].value;
+								n = f[i].name;
+								info[C][n]= v ;
+
+							if (f[i].value != "") {
+							};
+
+						};
+				
+							 
+					
+				};
 			};
+			//	store.set('info', info);
+			//	get = store.get('info');
+				//console.log(get);
+			
+			console.log(info)
+		
 			return	true;
 		};
 
